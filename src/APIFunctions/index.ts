24423c433be9899007bae99a947e0ba216df8032
Aaron Deas <deas.aaron@gmail.com>
2022-01-30 18:54:36 -0500

    Made it so that update route can be passed custom data via req.body

--- src/APIFunctions/index.ts
+++ src/APIFunctions/index.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
-
+interface FormFields {
+  to: string;
+  data: {string: { string: string }}[],
+  fieldsToReturn: number[];
+}
 class APIFunctions {
   constructor() {}
   getApp() {
@@ -17,42 +21,13 @@ class APIFunctions {
     });
     return response;
   }
-  async updateField(body) {
+  async updateField(body: FormFields) {
     const headers = {
       'QB-Realm-Hostname': 'piedmontplumbers.quickbase.com',
       'User-Agent': 'Test',
       Authorization: `QB-USER-TOKEN ${process.env.QB_USER_TOKEN}`,
       'Content-Type': 'application/json'
-    };
-    // let body = {
-    //   to: 'br4vj56h7',
-    //   data: [
-    //     {
-    //       '6': {
-    //         value: 'Test Location',
-    //       },
-    //       '7': {
-    //         value: 'Test Phase',
-    //       },
-    //       '8': {
-    //         value: 'Test Category',
-    //       },
-    //       '9': {
-    //         value: 'Test Subcategory',
-    //       },
-    //       '10': {
-    //         value: 'Test Item',
-    //       },
-    //       '11': {
-    //         value: 'Test Qty',
-    //       },
-    //       '12': {
-    //         value: 'Added from API',
-    //       },
-    //     },
-    //   ],
-    //   fieldsToReturn: [6, 7, 8, 9, 10, 11, 12],
-    // };
+    };    
     const response = await axios({
       method: 'POST',
       url: 'https://api.quickbase.com/v1/records',
--- src/controllers/homeRoutes.ts
+++ src/controllers/homeRoutes.ts
@@ -14,6 +14,7 @@ router.get('/', (req: Request, res: Response) => {
 router.post('/update', async (req: Request, res: Response) => {
   try {
     await res.status(200).json(APIController.updateField(req.body));
+    console.log('Record Inserted')
   } catch (e) {
     res.status(500).json({
       message: 'Error',
